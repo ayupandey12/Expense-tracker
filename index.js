b1eeec8325ef89241bf06f86c2e4b02652897de7
ayupandey12 <ayushpandey9802@gmail.com>
2026-06-29 09:27:27 +0530

    make program using commander

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+import process from "node:process"
+import { program } from "commander"
+program.command("add").description("add expense")
+.requiredOption("--description <text>","add description")
+.requiredOption("--amount <number>","add amount")
+.action((option)=>{
+  console.log(option.amount,option.description)
+})
+program.command("delete").description("delete the expense with given id")
+.requiredOption("--id <number>","give the id")
+.action((option)=>{
+  console.log(option.id)
+})
+program.command("list")
+.description("list of expenses")
+.action(()=>{
+    console.log("ok");
+})
+program.command("summary")
+.description("total expense")
+.action(()=>{
+    console.log("ok1");
+})
+
+program.parse(process.argv)
